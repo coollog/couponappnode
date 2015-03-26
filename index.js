f4ab2aec703aff2a1089159543ed200427d5c969
Qingyang Chen <qingyang.chen@gmail.com>
2015-03-26 18:19:30 -0400

    setup new framework structure and separated out process and connect

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-var app = require('express')(),
-    http = require('http').Server(app),
-    server = require('./server');
-
-var port = 8888;
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-server(http);
-
-http.listen(port, function () {
-  console.log('listening on *:' + port);
-});
+var http   = require('./server/http'),
+    socket = require('./server/socket');

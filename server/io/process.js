563a3bdb42984032b432cc4be4e33a3238f1d065
Qingyang Chen <qingyang.chen@gmail.com>
2015-03-30 12:54:43 -0400

    wrote framework for socket.io

--- server/io/process.js
+++ server/io/process.js
@@ -2,7 +2,57 @@
 // (though can partition into multiple files)
 
 module.exports = function(server, socket) {
-  socket.on('register', function (data) {
+  // Customer side
+
+  socket.on('customer login', function (data) {
+
+  });
+
+  socket.on('customer register', function (data) {
+    
+  });
+
+  socket.on('deals', function (data) {
+    
+  });
+
+  socket.on('deal', function (data) {
+    
+  });
+
+  socket.on('search deals', function (data) {
+    
+  });
+
+  socket.on('claim deal', function (data) {
+    
+  });
+
+  socket.on('customer deals', function (data) {
+    
+  });
+
+
+  // Business side
+
+  socket.on('business login', function (data) {
+    
+  });
+
+  socket.on('business register', function (data) {
+    
+  });
+
+  socket.on('put company', function (data) {
+    
+  });
+
+  socket.on('put deal', function (data) {
+    
+  });
+
+  socket.on('business deals', function (data) {
+    
   });
 
   socket.on('disconnect', function () {

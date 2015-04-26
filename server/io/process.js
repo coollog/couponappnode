e75642d90e2557e61a61214f5340d273acabed89
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-26 12:51:04 -0400

    fixing payment

--- server/io/process.js
+++ server/io/process.js
@@ -217,8 +217,10 @@ module.exports = function(server, socket, stripe) {
             server.db['customers'].findOneAndUpdate({
               _id: socket.user._id
             }, {
-              stripeid: stripeid,
-              striperedacted: data.redacted
+              $set: {
+                stripeid: stripeid,
+                striperedacted: data.redacted
+              }
             }, function (err, doc) {
               if (err == null && doc != null) succeed(stripeid);
               else fail('could not update user');

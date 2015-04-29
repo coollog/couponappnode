2fd33cfb3d6a80d0a57e35c3642144b51ba7b64b
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-28 21:01:05 -0400

    customer edit does not get rid of claimed field

--- server/io/process.js
+++ server/io/process.js
@@ -105,7 +105,9 @@ module.exports = function(server, socket, stripe) {
 
       server.db['customers'].findOneAndUpdate({
         _id: socket.user._id
-      }, data, function (err, r) {
+      }, {
+        $set: data
+      }, function (err, r) {
         if (err == null && doc != null) succeed();
         else fail();
       });

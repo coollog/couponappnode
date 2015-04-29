8e1593270651d7acf6db3ae413b82b6fe1e4e227
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-28 21:35:07 -0400

    fixed tests

--- test/test2.js
+++ test/test2.js
@@ -1,31 +1,32 @@
 module.exports = function (socket, db, callback) {
-    // Test 2 -- Customer Register fail //
+  // Test 2 -- Customer Register fail //
 
-    var response = 0
-    var message = { "email": "jchirik@gmail.com", "password": "test", "firstname": "John", "lastname": "Apples" };
+  var response = 0;
+  var message = {
+    "email": "jchirik@gmail.com",
+    "password": "test",
+    "firstname": "John",
+    "lastname": "Apples"
+  };
+  socket.emit('customer register', message);
+
+  // try after email has already been created
+  socket.on('customer register succeed', function() {
     socket.emit('customer register', message);
-
-    // try after email has already been created
-    socket.on('customer register succeed', function() {
-        socket.emit('customer register', message);
-        socket.on('customer register fail', function(err) {
-            if (!response) {
-                response = 1
-                // check db here?
-                console.log('Test 2 passed ..... attempted customer register (email already taken)')
-                callback()
-                return this
-            }
-        })
-        socket.on('customer register succeed', function() {
-            if (!response) {
-                response = 1
-                console.log('Test 2 failed: should not have succeeded')
-                callback()
-                return this
-            }
-        }) 
+    socket.on('customer register fail', function (err) {
+      if (!response) {
+        response = 1;
+        // check db here?
+        console.log('Test 2 passed ..... attempted customer register (email already taken)');
+        callback();
+      }
     })
-
-    // End Test 2 //
+    socket.on('customer register succeed', function() {
+      if (!response) {
+        response = 1;
+        console.log('Test 2 failed: should not have succeeded');
+        callback();
+      }
+    });
+  });
 }
--- test/test3.js
+++ test/test3.js
@@ -1,38 +1,39 @@
 module.exports = function (socket, db, callback) {
-    // Test 3 - Customer Login pass //
+  // Test 3 - Customer Login pass //
 
-    var response = 0
-    var message = { "email": "jchirik@gmail.com", "password": "test" };
-    socket.emit('customer register', message);
+  var response = 0;
+  var message = {
+    "email": "jchirik@gmail.com",
+    "password": "test"
+  };
+  socket.emit('customer register', message);
 
-    // login after registered
-    socket.on('customer register succeed', function() {
-        socket.emit('customer login', message);
-        socket.on('customer login fail', function(err) {
-            if (!response) {
-                response = 1
-            	console.log('Test 3 failed: ', err)
-                callback()
-                return this
-            }
-        })
-        socket.on('customer login succeed', function() {
-            if (!response) {
-                response = 1
-                // check database for customer information
-                db.collection('customers').findOne(message, function(err, doc) {
-                    if (err == null && doc != null) {
-                        console.log('Test 3 passed ..... existing customer login')
-                        callback()
-                    } else {
-                        console.log('Test 3 failed: ', err)
-                        callback()
-                    }
-                })
-                return this
-            }
-        }) 
-    })
+  // login after registered
+  socket.on('customer register succeed', function() {
+    socket.emit('customer login', message);
 
-    // End Test 3 //
+    socket.on('customer login fail', function (err) {
+      if (!response) {
+        response = 1;
+        console.log('Test 3 failed: ', err);
+        callback();
+      }
+    });
+
+    socket.on('customer login succeed', function() {
+      if (!response) {
+        response = 1;
+        // check database for customer information
+        db.collection('customers').findOne(message, function (err, doc) {
+          if (err == null && doc != null) {
+            console.log('Test 3 passed ..... existing customer login');
+            callback();
+          } else {
+            console.log('Test 3 failed: ', err);
+            callback();
+          }
+        });
+      }
+    });
+  });
 }

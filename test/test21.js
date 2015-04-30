690bc2108167e924f5b607b0c5d9416fffcdbf43
Shona Hemmady <shonah@gmail.com>
2015-04-30 17:31:24 -0400

    last of tests

--- test/test21.js
+++ test/test21.js
@@ -1,29 +1,57 @@
-module.exports = function (socket, db, callback, fail, pass) {
-  // Test 19 - Business View Deals pass //
-  // includes: business login, business add deal, business view deals
+module.exports = function (socket, db, callback) {
+    // Test 6 - Add card pass //
 
-  var response = 0;
-  var testhelpers = require('./testhelpers.js')(socket, db);
+    var config = require('../config.js')
+    var stripe = require('stripe')(config.stripekey);
+    var response = 0
+    var message = { email: "jchirik@gmail.com", password: "test", firstname: "John", lastname: "Apples" };
 
-  // register and login business, then create some deals
-  testhelpers.businessLogin(function () {
-    // add one deal
-    testhelpers.createDeal(function () {
+    socket.emit('customer register', message);
 
-          socket.emit('business deals');
-          socket.on('business deals', function(deals) {
-            if (!response) {
-              response = 1;
-              // check that one was added
-              // console.log(socket.user._id)
-              console.log(deals)
-              if (deals.length == 1)
-                pass('business view list of deals', callback);
-              else
-                fail('business view list of deals wrong #', callback);
+    // update after customer registered and logged in
+    socket.on('customer register succeed', function() {
+        var token = stripe.tokens.create({
+            card: {
+                number: 4242424242424242,
+                cvc: 123,
+                exp_month: 12,
+                exp_year: 2018
             }
-          });
-
+        }, function(err, tok) {
+            console.log(token)
+            socket.emit('stripe token', tok)
+        })
+        
+        // if card addition is unsuccessful
+        socket.on('stripe token fail', function(err) {
+            if (!response) {
+                response = 1
+                console.log('Test 6 failed: ', err)
+                callback()
+                return this
+            }
+        })
+        // if card addition is successful
+        socket.on('stripe token succeed', function () {
+            if (!response) {
+                response = 1
+                // check database
+                db.collection('customers').findOne(message, function(err, doc) {
+                    if (err == null && doc != null) {
+                        // FIND KEYS AND DO THINGS TO CHECK THAT THEY ARE SAME AS THE MESSAGE!!!!
+                        //   not done with this test yet!!KOW)ER)($#(*))
+                        console.log('Test 6 passed ..... existing customer update')
+                        callback()
+                    } else {
+                        // console.log(err + ' and doc: ' + doc)
+                        console.log('Test 6 failed: ', err)
+                        callback()
+                    }
+                }); 
+            }
+        });
+        
     });
-  });
-};
+
+    // End Test 5 //
+}
